b136957f9b69efec83aa4ae4de3af64a87ce0aae
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-07-07 07:19:28 +0300

    factored xread.h xwrite.h srpmblob.h

--- compress-srclist.c
+++ compress-srclist.c
@@ -1,120 +1,15 @@
-#include <stdbool.h>
-#include <assert.h>
-#include <limits.h>
-#include <errno.h>
-#include <unistd.h>
-
-static ssize_t xread(int fd, void *buf, size_t size)
-{
-    assert(size);
-    assert(size <= SSIZE_MAX);
-
-    ssize_t total = 0;
-    do {
-	ssize_t ret = read(fd, buf, size);
-	if (ret < 0) {
-	    if (errno == EINTR)
-		continue;
-	    return -1;
-	}
-	if (ret == 0)
-	    break;
-	assert((size_t) ret <= size);
-	buf = (char *) buf + ret;
-	size -= ret;
-	total += ret;
-    } while (size);
-
-    return total;
-}
-
-static bool xwrite(int fd, const void *buf, size_t size)
-{
-    assert(size);
-    bool zero = false;
-    do {
-	ssize_t ret = write(fd, buf, size);
-	if (ret < 0) {
-	    if (errno == EINTR)
-		continue;
-	    return false;
-	}
-	if (ret == 0) {
-	    if (zero) {
-		// write keeps returning zero
-		errno = EAGAIN;
-		return false;
-	    }
-	    zero = true;
-	    continue;
-	}
-	zero = false;
-	assert(ret <= size);
-	buf = (char *) buf + ret;
-	size -= ret;
-    } while (size);
-
-    return true;
-}
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <rpm/rpmtag.h>
-#include <t1ha.h>
-
-struct srpmBlob {
-    void *blob;
-    size_t blobSize;
-    const char *pkgname;
-    uint64_t hash;
-};
-
-static bool read1(struct srpmBlob *b)
-{
-    unsigned lead[4];
-    ssize_t ret = xread(0, lead, sizeof lead);
-    if (ret == 0)
-	return false;
-    assert(ret == sizeof lead);
-
-    const unsigned char magic[8] = { 0x8e, 0xad, 0xe8, 0x01, 0x00, 0x00, 0x00, 0x00 };
-    assert(memcmp(lead, magic, 8) == 0);
-
-    unsigned il = ntohl(lead[2]);
-    unsigned dl = ntohl(lead[3]);
-    assert(il-1 < (64<<10));
-    assert(dl-1 < (16<<20));
-
-    b->blobSize = 16 + 16 * il + dl;
-    b->blob = malloc(b->blobSize);
-    assert(b->blob);
-    memcpy(b->blob, lead, 16);
-    ret = xread(0, b->blob + 16, b->blobSize - 16);
-    assert(ret == b->blobSize - 16);
-
-    struct HeaderEntry {
-	int tag;
-	int type;
-	unsigned off;
-	unsigned cnt;
-    };
-    struct HeaderEntry *ee = b->blob + 16;
-    assert(il > 1);
-    assert(ee[1].tag == htonl(RPMTAG_NAME));
-    assert(ee[1].type == htonl(RPM_STRING_TYPE));
-    unsigned off = ntohl(ee[1].off);
-    assert(off < dl);
-    char *data = (void *) (ee + il);
-    b->pkgname = &data[off];
-    b->hash = t1ha(b->pkgname, strlen(b->pkgname), 0);
-    return true;
-}
-
-#include <stdio.h>
+#include <assert.h>
 #include <inttypes.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <t1ha.h>
 #include <zstd.h>
+#include "xread.h"
+#include "xwrite.h"
+#include "srpmblob.h"
 
 int main(int argc, char **argv)
 {
@@ -176,10 +71,10 @@ int main(int argc, char **argv)
 	size_t totalSize = stack[0].blobSize;
 	for (size_t i = 1; i < n; i++)
 	    totalSize += stack[i].blobSize;
-	size_t pkgnameoff = stack[0].pkgname - (char *) stack[0].blob;
+	size_t nameoff = stack[0].name - (char *) stack[0].blob;
 	stack[0].blob = realloc(stack[0].blob, totalSize + ZSTD_COMPRESSBOUND(totalSize));
 	assert(stack[0].blob);
-	stack[0].pkgname = stack[0].blob + pkgnameoff;
+	stack[0].name = stack[0].blob + nameoff;
 
 	void *p = stack[0].blob + stack[0].blobSize;
 	for (int i = 1; i < n; i++)
@@ -201,9 +96,9 @@ int main(int argc, char **argv)
 
 	uint64_t hi;
 	uint64_t lo = t1ha2_atonce128(&hi, p, zsize, 0);
-	fprintf(stderr, "%016" PRIx64 "%016" PRIx64 "\t%zu\t%s", lo, hi, zsize, stack[0].pkgname);
+	fprintf(stderr, "%016" PRIx64 "%016" PRIx64 "\t%zu\t%s", lo, hi, zsize, stack[0].name);
 	for (size_t i = 1; i < n; i++) {
-	    fprintf(stderr, " %s", stack[i].pkgname);
+	    fprintf(stderr, " %s", stack[i].name);
 	    free(stack[i].blob);
 	}
 	fprintf(stderr, "\n");
@@ -215,18 +110,18 @@ int main(int argc, char **argv)
 
 #if 0
     // One header per chunk.
-    while (read1(stack))
+    while (readSrpmBlob(stack))
 	Pop(++nstack);
 #else
     // 2+ headers per chunk.
-    while (read1(&stack[nstack])) {
+    while (readSrpmBlob(&stack[nstack])) {
 	nstack++;
 	switch (nstack) {
 	case 1:
 	    break;
 	case 2:
 	case 3:
-	    if (stack[nstack-1].hash < stack[nstack-2].hash)
+	    if (stack[nstack-1].nameHash < stack[nstack-2].nameHash)
 		break;
 	    // fall through
 	case 4:
