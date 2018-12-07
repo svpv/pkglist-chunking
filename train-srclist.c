ca7c9fedefd503c306aac75243be8d6eb38a4e1e
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-12-07 23:32:29 +0300

    train-srclist.c: sync with chunk-srclist.c

--- train-srclist.c
+++ train-srclist.c
@@ -12,46 +12,42 @@ struct samples {
 
 static void load(void)
 {
-    struct srpmBlob stack[8];
-    size_t nstack = 0;
+    struct srpmBlob q[8];
+    size_t nq = 0;
 
     void *p = samples.samplesBuffer;
 
     void Pop(size_t n)
     {
 	assert(n > 0);
-	assert(nstack >= n);
+	assert(nq >= n);
 
 	void *p0 = p;
 	for (size_t i = 0; i < n; i++) {
-	    p = mempcpy(p, stack[i].blob, stack[i].blobSize);
-	    free(stack[i].blob);
+	    p = mempcpy(p, q[i].blob, q[i].blobSize);
+	    free(q[i].blob);
 	}
 	samples.samplesSizes[samples.nbSamples++] = p - p0;
 
-	nstack -= n;
-	memmove(stack, stack + n, nstack * sizeof stack[0]);
+	nq -= n;
+	memmove(q, q + n, nq * sizeof q[0]);
     }
 
-    while (readSrpmBlob(&stack[nstack])) {
-	nstack++;
-	switch (nstack) {
-	case 1:
-	    break;
-	case 2:
-	case 3:
-	    if (stack[nstack-1].nameHash < stack[nstack-2].nameHash)
-		break;
-	    // fall through
-	case 4:
-	    Pop(nstack);
-	    break;
-	default:
-	    assert(!"possible");
+    while (readSrpmBlob(&q[nq])) {
+	nq++;
+	switch (nq) {
+	case 1: case 2: break;
+	case 3: if (q[1].nameHash > q[2].nameHash) Pop(2); break;
+	case 4: if (q[2].nameHash > q[3].nameHash) Pop(3); break;
+	case 5: if (q[3].nameHash > q[4].nameHash) Pop(4); break;
+	case 6: if (q[4].nameHash > q[5].nameHash) Pop(5); break;
+	case 7: if (q[5].nameHash > q[6].nameHash) Pop(6); break;
+	case 8: if (q[6].nameHash > q[7].nameHash) Pop(7); else Pop(8); break;
+	default: assert(!"possible");
 	}
     }
-    if (nstack)
-	Pop(nstack);
+    if (nq)
+	Pop(nq);
 }
 
 int main()
