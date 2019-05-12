185c6304f3de7d592b389f94b2996052d0b0f102
Alexey Tourbin <alexey.tourbin@gmail.com>
2019-05-12 08:42:12 +0300

    train-pkglist.c: expose more options

--- train-pkglist.c
+++ train-pkglist.c
@@ -57,20 +57,24 @@ int main(int argc, char **argv)
     assert(!isatty(1));
 
     int level = 19;
-    int verbose = 2;
+    int verbose = 1;
+    bool fast = false;
+    int d = 0, k = 0;
 
     enum {
 	OPT_HELP = 256,
 	OPT_SEED,
+	OPT_FAST,
     };
     static const struct option longopts[] = {
 	{ "help",    no_argument,       NULL, OPT_HELP },
 	{ "verbose", no_argument,       NULL, 'v' },
 	{ "seed",    required_argument, NULL, OPT_SEED },
+	{ "fast",    no_argument,       NULL, OPT_FAST },
 	{  NULL,     0,                 NULL,  0  },
     };
     int opt;
-    while ((opt = getopt_long(argc, argv, "1::2::3::4::5::6::7::8::9::v", longopts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "1::2::3::4::5::6::7::8::9::d:k:v", longopts, NULL)) != -1) {
 	switch (opt) {
 	case '1':
 	    if (!optarg)
@@ -87,12 +91,23 @@ int main(int argc, char **argv)
 		assert(!"supported levels > 19");
 	    level = opt - '0';
 	    break;
+	case 'd':
+	    d = atoi(optarg);
+	    assert(d > 0);
+	    break;
+	case 'k':
+	    k = atoi(optarg);
+	    assert(k > 0);
+	    break;
 	case 'v':
 	    verbose++;
 	    break;
 	case OPT_SEED:
 	    assert(*optarg >= '0' && *optarg <= '9');
 	    rpmBlobSeed = strtoull(optarg, NULL, 0);
+	    break;
+	case OPT_FAST:
+	    fast = 1;
 	    break;
 	default:
 	    fprintf(stderr, "Usage: %s <pkglist >dict\n", argv[0]);
@@ -103,17 +118,42 @@ int main(int argc, char **argv)
     load();
 
     ZDICT_cover_params_t params = {
-	.d = 6,
+	.d = d, .k = k,
 	.nbThreads = 2,
 	.zParams.compressionLevel = level,
 	.zParams.notificationLevel = verbose,
     };
+    ZDICT_fastCover_params_t fastParams = {
+	.d = d, .k = k,
+	.nbThreads = 2,
+	.zParams.compressionLevel = level,
+	.zParams.notificationLevel = verbose,
+	.splitPoint = 1.0,
+    };
     char dict[512<<10];
-    size_t dictSize = ZDICT_optimizeTrainFromBuffer_cover(dict, sizeof dict,
-	    samples.samplesBuffer, samples.samplesSizes, samples.nbSamples,
-	    &params);
-    assert(dictSize == sizeof dict);
-    fprintf(stderr, "best parameters: d=%u k=%u\n", params.d, params.k);
+    size_t dictSize;
+    if (fast) {
+	if (d && k)
+	    dictSize = ZDICT_trainFromBuffer_fastCover(dict, sizeof dict,
+		samples.samplesBuffer, samples.samplesSizes, samples.nbSamples, fastParams);
+	else {
+	    dictSize = ZDICT_optimizeTrainFromBuffer_fastCover(dict, sizeof dict,
+		samples.samplesBuffer, samples.samplesSizes, samples.nbSamples, &fastParams);
+	    fprintf(stderr, "best parameters: d=%u k=%u\n", fastParams.d, fastParams.k);
+	}
+	assert(dictSize >= (64<<10) && dictSize <= sizeof dict);
+    }
+    else {
+	if (d && k)
+	    dictSize = ZDICT_trainFromBuffer_cover(dict, sizeof dict,
+		samples.samplesBuffer, samples.samplesSizes, samples.nbSamples, params);
+	else {
+	    dictSize = ZDICT_optimizeTrainFromBuffer_cover(dict, sizeof dict,
+		samples.samplesBuffer, samples.samplesSizes, samples.nbSamples, &params);
+	    fprintf(stderr, "best parameters: d=%u k=%u\n", params.d, params.k);
+	}
+	assert(dictSize == sizeof dict);
+    }
 
     if (!xwrite(1, dict, dictSize))
 	assert(!!!"write failed");
